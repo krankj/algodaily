d56651bd623666a8aa1474fa29ee6fb27f4ce2fa
Sudarshan K J <kjsudi@gmail.com>
2021-03-24 22:27:20 +0530

    Started working out on new problem

---
+++ others/3.isDollarDeleteEqual.js
@@ -0,0 +1,18 @@
+require("colors");
+
+/*
+You're given an array of strings containing alphabetical characters and certain $ characters. A $ represents a DELETE action whereby the character before it is deleted.
+
+Each of these strings will be run through a method to operate on the $ DELETE action. We want to find out if the final string is the same for all of them. Let's take an example:
+
+const input = ["f$st", "st"]
+isDollarDeleteEqual(input);
+// true
+// true because both become "st"
+*/
+
+function isDollarDeleteEqual(arr) {
+  return;
+}
+
+console.log(`Output: ${isDollarDeleteEqual(["f$st,st"])}`);
