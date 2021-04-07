13f7bb970cf45bf8475e78ae3da4602b7ca16358
Sudarshan K J <kjsudi@gmail.com>
2021-04-07 09:07:07 +0530

    [UPDATED] Added a very time efficient solution to the missing number problem

--- others/6.missingNumberInUnsorted.js
+++ others/6.missingNumberInUnsorted.js
@@ -1,7 +1,5 @@
 require("colors");
 
-//lowerBound is 11
-//upperBound is 19
 function missingNumberArrSoln(arr, lowerBound, upperBound) {
   // its consecutive, you know the length. You know one is missing. You just need to find which one it is
 
@@ -19,18 +17,23 @@ function missingNumberArrSoln(arr, lowerBound, upperBound) {
   }
 }
 
-function missinNumberMathSoln(arr, lB, uB) {
+function missingNumberMathSoln(arr, lb, ub) {
+  // sum of n consecutive numbers starting from n is n(n+1)/2. Sum of n numbers starting from i and ending at j is n(i+j)/2, where n is (j-i+1)
   let sum = 0;
-  let range = uB - lB + 1;
-  let diff = lB * uB + (range * (range + 1)) / 2;
-  console.log("Difference is", diff);
-  // sum of fist n numbers are n(n+1) / 2
+  let n = ub - lb + 1;
+  let diff = ((lb + ub) * n) / 2;
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
   return diff - sum;
 }
 
-const arr1 = [2, 5, 1, 4, 9, 6, 3, 7];
-//console.log(`Output: ${missingNumberArrSoln(arr1, 11, 19)}`);
-console.log(`Output: ${missinNumberMathSoln(arr1, 1, 9)}`);
+const arr1 = [12, 15, 11, 14, 19, 16, 13, 17];
+
+console.time("brute");
+console.log(`Output: ${missingNumberArrSoln(arr1, 11, 19)}`);
+console.timeEnd("brute");
+
+console.time("math");
+console.log(`Output: ${missingNumberMathSoln(arr1, 11, 19)}`);
+console.timeEnd("math");
