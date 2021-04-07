6d4b1ca59ff4a3a6fee79a0310f971faf30a26a5
Sudarshan K J <kjsudi@gmail.com>
2021-04-07 09:31:50 +0530

    Restructure code to match algodaily daily challenge sequence

---
+++ others/2.reachable.js
@@ -0,0 +1,72 @@
+require("colors");
+
+/*
+While going to solve the substring problem, ended up doing this :D
+Well this method is useful if you wanna check if there is a word/path is reachable. Something like this: 
+"abracadarbaaaa", "bar"
+
+"bar" can be formed from the above since "ba", and then from "a" you can go to "r"
+Can be used for something like connecting flights
+*/
+function Node(value) {
+  this.value = value;
+  this.next = [];
+}
+
+class List {
+  constructor() {
+    this.counter = 0;
+    this.map = new Map();
+    this.tail = new Node();
+  }
+  add(value) {
+    let node;
+    if (this.map.has(value)) {
+      node = this.map.get(value);
+    } else {
+      node = new Node(value);
+      this.map.set(value, node);
+    }
+    this.tail.next.push(node);
+    this.tail = node;
+  }
+  getMap() {
+    return this.map;
+  }
+}
+
+const subString = (str, subStr) => {
+  let list = new List();
+  for (let c of str) {
+    list.add(c);
+  }
+
+  let map = list.getMap();
+  let node;
+  for (let i = 0; i < subStr.length; i++) {
+    if (i === 0) {
+      if (map.has(subStr[0])) {
+        node = map.get(subStr[0]);
+      } else {
+        return "Not Found";
+      }
+    } else {
+      let found = false;
+      for (let linkNode of node.next) {
+        if (linkNode.value === subStr[i]) {
+          node = linkNode;
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        return "Not Found";
+      }
+    }
+  }
+  return "Found";
+};
+
+console.log("Output: ", subString("abracadarbaaaa", "bar"));
+console.log("Output: ", subString("arrarr", "arr"));
+console.log("Output: ", subString("Srinivas", "nin"));
